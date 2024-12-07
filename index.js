f8725fb51b4341aed6f0b0e2f094c26251e31aaf
Lordshiva12 <khushalsains735@gmail.com>
2024-12-07 19:06:30 +0530

    update

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-<<<<<<< HEAD
 import cors from "cors";
-import userRoutes from './routes/userRoutes';
-=======
 import userRoutes from './routes/userRoutes.js';
 import connectDB from "./Config/dbConfig.js";
 import bodyParser from "body-parser";
->>>>>>> a2fe34a2fe07f23c88f2b0e57ab09d235818c48d
 
 const app = express();
 const PORT = process.env.PORT || 3000;
